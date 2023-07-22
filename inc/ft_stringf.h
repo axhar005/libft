dbaaa2535b225da60e9da58867e408b278b3fa0c
olivier boucher <olivierboucher@mbp-de-olivier.home>
2023-07-22 16:14:40 -0400

    update .h again again

--- inc/ft_stringf.h
+++ inc/ft_stringf.h
@@ -6,7 +6,7 @@
 /*   By: olivierboucher <olivierboucher@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 13:23:16 by olivierbouc       #+#    #+#             */
-/*   Updated: 2023/07/22 15:36:59 by olivierbouc      ###   ########.fr       */
+/*   Updated: 2023/07/22 16:13:54 by olivierbouc      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,15 @@
 # define FT_STRINGF_H
 
 # include "conversion.h"
-# include "get_next_line.h"
 # include "error.h"
+# include "get_next_line.h"
 # include "memory.h"
 # include "string.h"
 # include <stdarg.h>
 # include <unistd.h>
 
 // prototype
+
+char	*ft_stringf(const char *str, ...);
 
 #endif
